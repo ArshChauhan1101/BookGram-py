a6f94e034e8d531d402cbec9c47828b89d8bffae
ArshChauhan1101 <achauhan4830@conestogac.on.ca>
2023-11-14 10:17:42 -0500

    Updated code to get Json File

--- authetication/util.py
+++
@@ -1,9 +0,0 @@
-#loging and registration
-
-class LoginForm(FlaskForm):
-    username = StringField('Username', id = 'username_loginID', validators = [DataRequired()])
-    passoword = PassowordFiled('Passoword', id = 'pass_login', validators = [DataRequired()])
-
-
-class CreateAccountForm(FlaskForm):
-    
--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-import requests # imported requests from after installing googleAPI
+import requests
+import json
 
-def get_books(api_key, query, max_results=20): #defining the function name getting books
-    base_url = "https://www.googleapis.com/books/v1/volumes" #Base URL of the google books which will find all the books from API
-    # Parameters which are used to get the data q - string which will check, key - it is the api_key which is being used to autheticate and maxResults which will give us max books we want
-    params = { 
+def get_books(api_key, query, max_results=20):
+    base_url = "https://www.googleapis.com/books/v1/volumes"
+    params = {
         'q': query,
         'key': api_key,
         'maxResults': max_results
     }
 
-    response = requests.get(base_url, params=params) 
+    response = requests.get(base_url, params=params)
     data = response.json()
 
     if 'items' in data:
@@ -19,17 +19,35 @@ def get_books(api_key, query, max_results=20): #defining the function name getti
 
 if __name__ == "__main__":
     api_key = "API_KEY"
-    query = input("Enter a book title: ")
+    query = input("Enter a book title or Author Name: ")
 
     books = get_books(api_key, query)
 
     if books:
+        output_data = []  # List to store book data
+
         for book in books:
             volume_info = book['volumeInfo']
             title = volume_info['title']
             authors = volume_info.get('authors', ['Unknown'])
             published_date = volume_info.get('publishedDate', 'Unknown')
             book_category = volume_info.get('categories', ['Unknown'])
-            print(f"Title: {title}, Authors: {', '.join(authors)}, Published Date: {published_date}, Category: {', '.join(book_category)}")
+
+            # Create a dictionary for each book
+            book_data = {
+                "Title": title,
+                "Authors": authors,
+                "Published Date": published_date,
+                "Category": book_category
+            }
+
+            output_data.append(book_data)
+
+        # Save the data to a JSON file
+        output_file_path = "books_output.json"
+        with open(output_file_path, 'w') as json_file:
+            json.dump(output_data, json_file, indent=2)
+
+        print(f"Books data has been saved to {output_file_path}")
     else:
         print("No books found.")
